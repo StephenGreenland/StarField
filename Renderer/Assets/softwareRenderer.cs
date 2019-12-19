e86547be2acd9823e94968942008a11ab8cc27ad
Stephen Greenland <1013573@student.sae.edu.au>
2019-12-19 23:06:15 +1100

    Project Commit

---
+++ Renderer/Assets/softwareRenderer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class softwareRenderer : MonoBehaviour
+{
+    
+
+    public int xSize;
+    public int ySize;
+    private int zeroX;
+    private int zeroY;
+
+    public byte[,] backBuffer;
+
+    private float timer;
+    
+    public GameObject quadRenderer;
+
+
+    private Texture2D tex;
+    public Vector3 star;
+    public List<Vector3> stars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = .5f;
+        
+      // backBuffer = new byte[xSize,ySize];
+
+      zeroX = xSize / 2;
+      zeroY = ySize / 2;
+      
+        
+        tex = new Texture2D(xSize,ySize,TextureFormat.RGB24,false);
+        tex.filterMode = FilterMode.Point;
+        gameObject.GetComponent<MeshRenderer>().material.mainTexture = tex;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Makestar();
+            StartCoroutine(MoveZ());
+            timer = .5f;
+        }
+        
+        drawThings();
+        
+    }
+    
+    IEnumerator MoveZ()
+    {
+        for (int i = 0; i < stars.Count; i++)
+        {
+            yield return new WaitForSeconds(1);
+
+            stars[i] = new Vector3(stars[i].x, stars[i].y, stars[i].z +1f);
+        }
+
+        yield return null;
+    }
+
+    private void Makestar()
+    {
+        stars.Add(new Vector3(UnityEngine.Random.Range((int)xSize/8 *-1,xSize/8),UnityEngine.Random.Range(ySize/8 *-1,ySize/8),0));
+    }
+
+    void drawThings()
+    {
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                tex.SetPixel(i,j,Color.black);
+            }
+        }
+
+        if (stars.Count > 0)
+        {
+            
+            for (int i = 0; i < stars.Count; i++)
+            {
+                
+                if (zeroX + (int) stars[i].x * (int) stars[i].z < xSize &&
+                    zeroX + (int) stars[i].x * (int) stars[i].z > xSize * -1)
+                {
+                    if (zeroY + (int) stars[i].y * (int) stars[i].z < ySize &&
+                        zeroY + (int) stars[i].y * (int) stars[i].z > ySize * -1)
+                    {
+                        tex.SetPixel(zeroX + (int) stars[i].x * (int) stars[i].z,
+                            zeroY + (int) stars[i].y * (int) stars[i].z, Color.white);
+
+                    }
+                }
+            }
+            
+        }
+        tex.Apply(true);
+    }
+    
+}
---
+++ Renderer/Assets/star.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class star : MonoBehaviour
+{
+
+    public softwareRenderer render;
+
+    public Vector3 starLocation;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        starLocation = new Vector3(Random.Range(render.xSize *-1,render.xSize),Random.Range(render.ySize *-1,render.ySize),0);
+      
+        render.stars.Add(starLocation);
+       
+        StartCoroutine(MoveZ());
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MoveZ()
+    {
+        for (int i = 0; i < 120; i++)
+        {
+            yield return new WaitForSeconds(1);
+            
+            starLocation = new Vector3(starLocation.x,starLocation.y,i);
+        }
+        
+        Destroy(this.gameObject);
+        yield return null;
+    }
+
+    private void OnDestroy()
+    {
+        render.stars.Remove(starLocation);
+    }
+}
